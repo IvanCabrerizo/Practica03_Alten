84cf8493a076d0c7455920dcb02853d665dd13e8
icabrerizo <ivan.cabrerizo@alten.es>
2023-05-08 09:52:29 +0200

    Fix in AndroidManifest.xml

--- app/src/main/java/com/example/practicas03/NavigationActivity.kt
+++ app/src/main/java/com/example/practicas03/NavigationActivity.kt
@@ -1,7 +1,7 @@
 package com.example.practicas03
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
