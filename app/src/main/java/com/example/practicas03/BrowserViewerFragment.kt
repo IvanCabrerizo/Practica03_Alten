2d29e2e0e430f02d8e8beadab26072421d87d935
icabrerizo <ivan.cabrerizo@alten.es>
2023-05-08 12:57:06 +0200

    Change GlobalScope for LifeCycleScope and more

--- app/src/main/java/com/example/practicas03/BrowserViewerFragment.kt
+++ app/src/main/java/com/example/practicas03/BrowserViewerFragment.kt
@@ -1,19 +1,22 @@
 package com.example.practicas03
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.practicas03.databinding.FragmentWebBrowserBinding
 
 class BrowserViewerFragment : Fragment() {
 
+    private val binding by lazy { FragmentWebBrowserBinding.inflate(layoutInflater) }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_browser_viewer, container, false)
+    ): View {
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
