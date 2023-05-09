0ac506b1a2e52325e14bf981ed380035283e0f0f
icabrerizo <ivan.cabrerizo@alten.es>
2023-05-09 14:21:37 +0200

    Adapter in progress

--- app/src/main/java/com/example/practicas03/adapter/WebBrowserListAdapter.kt
+++ app/src/main/java/com/example/practicas03/adapter/WebBrowserListAdapter.kt
@@ -1,2 +1,44 @@
 package com.example.practicas03.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.practicas03.data.model.WebBrowserBo
+import com.example.practicas03.databinding.ListBrowserItemBinding
+
+class WebBrowserListAdapter :
+    ListAdapter<WebBrowserBo, WebBrowserListAdapter.WebBrowserBoViewHolder>(
+        WebBrowserDiffUtilCallBack()
+    ) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WebBrowserBoViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val listBrowserItemBinding = ListBrowserItemBinding.inflate(layoutInflater, parent, false)
+        return WebBrowserBoViewHolder(listBrowserItemBinding)
+    }
+
+    override fun onBindViewHolder(
+        holder: WebBrowserListAdapter.WebBrowserBoViewHolder,
+        position: Int
+    ) {
+        holder.bind(getItem(position))
+    }
+
+    inner class WebBrowserBoViewHolder(listBrowserBinding: ListBrowserItemBinding) :
+        RecyclerView.ViewHolder(listBrowserBinding.root) {
+        fun bind(item: WebBrowserBo) {
+
+        }
+    }
+
+    private class WebBrowserDiffUtilCallBack : DiffUtil.ItemCallback<WebBrowserBo>() {
+        override fun areItemsTheSame(oldItem: WebBrowserBo, newItem: WebBrowserBo): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun areContentsTheSame(oldItem: WebBrowserBo, newItem: WebBrowserBo): Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+}
