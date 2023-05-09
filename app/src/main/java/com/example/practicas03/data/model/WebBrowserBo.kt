e8c8342c6f0723e4333aed2c6cc6eaf97bd25c6e
icabrerizo <ivan.cabrerizo@alten.es>
2023-05-09 08:33:46 +0200

    Created WebBrowserBo.kt and WebBrowserBoProvider.kt class, fixed a problem with removing the browser stack and added internet permissions to the manifest.

---
+++ app/src/main/java/com/example/practicas03/data/model/WebBrowserBo.kt
@@ -0,0 +1,22 @@
+package com.example.practicas03.data.model
+
+
+enum class CompatibleOperatingSystems(val operatingSystem: String) {
+    WINDOWS("Windows"),
+    MAC("Mac OS"),
+    LINUX("Linux"),
+    ANDROID("Android"),
+    IOS("iOS"),
+    WINDOWS_PHONE("Windows Phone")
+}
+
+
+data class WebBrowserBo(
+    val name: String,
+    val company: String,
+    val year: Int,
+    val logo: String,
+    val web: String,
+    val mobile: Boolean,
+    val compatible: List<CompatibleOperatingSystems>,
+)
